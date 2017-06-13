528842726c47f0164896316dd92de817ec8dbc70
Russ Gasdia <russ.gasdia@gmail.com>
2017-06-13 14:16:01 -0400

    Create master scrape

---
+++ vd17_master_scrape.R
@@ -0,0 +1,13 @@
+##### A Scrape for the VD17 Catalogue @ http://www.vd17.de###
+##### Developed by Russ Gasdia #####
+##### All questions and bugs should be directed to russell.gasdia@yale.edu #####
+
+
+###Initial Scrape: Gateway-Bayern
+     #https://opacplus.bib-bvb.de/TouchPoint_touchpoint/start.do?SearchProfile=Altbestand&SearchType=2
+     #Can iterate by year and gather VDN Numbers
+
+
+###Supplementary Srape: 
+     #https://gso.gbv.de/DB=1.28/
+     #Can iterate using VDN Numbers to gather more information on printers etc.
